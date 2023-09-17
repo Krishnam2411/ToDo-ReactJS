3e3f12cf36cff483e5e240ccf2cf1e2fe00b4e7f
krishnam <maheshwarikrishnam123@gmail.com>
2023-09-17 10:35:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import { useState } from "react";
 function App() {
+  const [list, setList] = useState(JSON.parse(localStorage.getItem('todo')) || []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main list={list} setList={setList} />
+      <Footer />
     </div>
   );
 }
